74772b7673e84e618efb388afa793e317cd4bb49
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-25 22:22:41 +0530

    Add Typography (#2)

---
+++ components/Typography/Heading4.tsx
@@ -0,0 +1,7 @@
+export function Heading4({ children }: { children: React.ReactNode }) {
+  return (
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      {children}
+    </h4>
+  );
+}
---
+++ components/Typography/index.tsx
@@ -0,0 +1,27 @@
+import { Blockquote } from "./Blockquote";
+import { Heading1 } from "./Heading1";
+import { Heading2 } from "./Heading2";
+import { Heading3 } from "./Heading3";
+import { Heading4 } from "./Heading4";
+import { InlineCode } from "./InlineCode";
+import { Large } from "./Large";
+import { Lead } from "./Lead";
+import { List } from "./List";
+import { Muted } from "./Muted";
+import { Paragraph } from "./Paragraph";
+import { Small } from "./Small";
+
+export {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  InlineCode,
+  Large,
+  Lead,
+  Muted,
+  Paragraph,
+  Small,
+  List,
+  Blockquote,
+};
